47734f257e318a9e931dcd5b46ddd9be44a0cc36
Otutochi <o.caleb.nwa@gmail.com>
2025-02-23 20:49:26 -0600

    added projects

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,22 @@ const App = () => {
       <h1>Easy Impact</h1>
       <h2>Impact made easy</h2>
 
-      <Project org='Wesley Foundation' loc="Saint Simon's Island" imgpath='\random.jpg' link='https://www.statewesley.org/' />
+      <div className="projects">
+        <Project org='Wesley Foundation' loc="Saint Simons, GA" imgpath='\wesley.png' link='https://www.statewesley.org/' />
+        <Project org='Cheyenne River Youth Project' loc="Eagle Butte, SD" imgpath='\cheyenne.jpg' link='http://www.lakotayouth.org/' />
+        <Project org='SoupMobile' loc="Dallas, TX" imgpath='\soup.jpg' link='http://www.soupmobile.org/' />
+        <Project org='Sci Starter' loc="Nationwide" imgpath='\scistarter.webp' link='http://www.arkidsread.org/' />
+        <Project org='AR Kids Read' loc="Pine Bluff, AR" imgpath='\arkidsread.webp' link='https://www.statewesley.org/' />
+        <Project org='Tutoring Chicago' loc="Chicago, IL" imgpath='\tchicago.jpg' link='http://www.tutoringchicago.org/' />
+        <Project org='EldersHelpers' loc="Austin, TX" imgpath='\elders.webp' link='https://elderhelpers.org/' />
+        <Project org='Federal Bureau of Prisons' loc="Fort Dix, NJ" imgpath='\prison.webp' link='https://volunteer.reentry.gov/Volunteer/s/' />
+        <Project org="SUN 'n FUN" loc="Lakeland, FL" imgpath='\sun.png' link='http://wwwflysnf.org/' />
+        <Project org='The ENGin Program' loc="New York, NY" imgpath='\eng.avif' link='http://www.enginprogram.org/' />
+        <Project org='Pacific Community Ventures' loc="San Francisco, CA" imgpath='\pacific.webp' link='https://www.pacificcommunityventures.org/' />
+        <Project org='Wheels For All' loc="Pie Bluff, AR" imgpath='\wheels.webp' link='http://wheelsforall.org/' />
 
+
+      </div>
       
     </div>
   )
